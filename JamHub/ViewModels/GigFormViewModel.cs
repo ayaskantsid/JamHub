9c8664570cb76aed66983975a57fcec2a6cc08a5
Ayaskant Siddharth <ishugzb1998@gmail.com>
2020-07-04 13:31:24 +0530

    Implement validation

--- JamHub/ViewModels/GigFormViewModel.cs
+++ JamHub/ViewModels/GigFormViewModel.cs
@@ -1,22 +1,31 @@
 ﻿using JamHub.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace JamHub.ViewModels
 {
     public class GigFormViewModel
     {
+        [Required]
         public string Venue { get; set; }
+
+        [Required]
+        [FutureDate]
         public string Date { get; set; }
+
+        [Required]
+        [ValidTime]
         public string Time { get; set; }
+
+        [Required]
         public byte Genre { get; set; }
+
         public IEnumerable<Genre> Genres { get; set; }
-        public DateTime DateTime
+
+        public DateTime GetDateTime()
         {
-            get
-            {
-                return DateTime.Parse(string.Format("{0} {1}", Date, Time));
-            }
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time));
         }
     }
 }
